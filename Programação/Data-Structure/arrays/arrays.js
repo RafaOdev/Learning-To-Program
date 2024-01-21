aca33233e767c51ce99b442a40814c7f9f075fb7
Rafael Oliveira <132023935+RafaOdev@users.noreply.github.com>
2024-01-21 13:13:12 -0300

    version1.0

---
+++ Programação/Data-Structure/arrays/arrays.js
@@ -0,0 +1,43 @@
+/* 
+     Array é uma estrutura de dado básica que na maioria das linguagens de programação armazena apenas um tipo de dado (INT, STRING, BOOLEANO e etc). O array armazena os seus elementos dentro de índices, esses índices ajuda a localizar o elemento dentro do array
+
+            EXEMPLO: let array = [10, 15, 1, 2, 23, 19];
+
+            foi criado um array em JavaScript com 6 elementos, cada elemento contém um índice que começa com 0 indo até o 5;
+            
+            EXEMPLO: array.push(25):
+
+            A função push é utilizada para adicionar um novo elemento no final do array, então após a adição do elemento 25 o array fica dessa forma [10, 15, 1, 2, 23, 19, 25];
+        
+            EXEMPLO: array.splice(0):
+
+            A função splice irá remover um elemento de sua escolha, no exemplo estamos removendo o elemento que se encontra no índice 0, que é o número 10, então o array ficaria [15, 1, 2, 23, 19, 25];
+
+            EXEMPLO: array.pop():
+
+            A função pop irá remover o último elemento do array, que no caso é o 25, então o array ficaria [15, 1, 2, 23, 19];
+
+            EXEMPLO: array.length:
+
+            array.length irá nos mostrar a quantidade de itens presentes dentro do array, que no caso seria 4;
+
+            EXEMPLO: array.unshift(30):
+
+            A função unshift irá adicionar um elemento ao primeiro índice, logo o array ficaria da seguinte forma, [30, 15, 1, 2, 23, 19];
+
+            EXEMPLO: array.shift():
+
+            A função shift irá remover o primeiro elemento do array, então o array ficaria [15, 1, 2, 23, 19];
+*/
+
+let array = [2,3,4,1,9,0];
+
+array.push(12); // adicionou 12 ao final do array;
+array.pop(); // removeu o último elemento do array;
+array.splice(3); // removeu o elemento do índice 3, que no caso é o número 1;
+array.unshift(25, 65); // adicionou os números 25 e 65 no começo do array;
+array.shift(); // remove o primeiro elemento do array, no caso o número 25;
+array.length; // mostra o tamanho do array;
+array[2] = 76; // adicionou o número 76 ao segundo índice do array;
+
+console.log(array);
