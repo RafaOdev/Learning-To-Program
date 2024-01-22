dce07674ada0549c1b086e8ade986f2cd7fd912b
Rafael Oliveira <132023935+RafaOdev@users.noreply.github.com>
2024-01-22 15:51:01 -0300

    Data-Structure
    This is an update on my data structure learning

---
+++ Programação/Learning-JavaScript/Data-Structure/stack/stack.js
@@ -0,0 +1,16 @@
+// STACK: A stack(pilha) é uma estrutura de dado que empilha os dados de forma sequêncial utilizando a regra LIFO(Last In First Out, último a entrar, primeiro a sair).Na pilha só é possível acessar um dado por vez a partir do último elemento adicionado a pilha. Para adicionar elementos a pilha é usado o método push e para retirar um elemento da fila é utilizado o elemento pop. Um exemplo de pilha pode ser dado a um algoritmo que recebe e armazena dados de livros, o primeiro livro a entrar dentro da pilha acabará se tornando a base dela, e todos os dados de outros livros serão armazenados sequencialmente acima desse primeiro livro que foi adicionado.
+
+/* POR QUE USAR PILHAS?
+   
+    A pilha por ser uma estrutura de dado que pode ser muito bem organizada, ela pode ser aplicada em diversas aplicações, como por exemplo para gerenciamento do fluxo do programa e também:
+
+    . Avaliação de expresões e parsing sintático;
+    . Algoritmos de Backtrancking;
+    . Gerencialmento de memória em tempo de compilação;
+    . Implementações em diversos algoritmos;
+    . Controlar a navegação de browsers;
+    . Endereçamento de instruções em microprocessadores;
+    . Análise de expressões aritméticas.
+*/
+
+
