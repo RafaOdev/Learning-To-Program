009fd0c36fc6087c47688cb636f2c42baccb72c5
Rafael Oliveira <132023935+RafaOdev@users.noreply.github.com>
2024-01-23 16:14:15 -0300

    Update stack.js

--- Programação/Learning-JavaScript/Data-Structure/stack/stack.js
+++ Programação/Learning-JavaScript/Data-Structure/stack/stack.js
@@ -2,7 +2,7 @@
 
 /* POR QUE USAR PILHAS?
    
-    A pilha por ser uma estrutura de dado que pode ser muito bem organizada, ela pode ser aplicada em diversas aplicações, como por exemplo para gerenciamento do fluxo do programa e também:
+    A pilha por ser uma estrutura de dado que pode ser muito bem organizada, ela pode ser aplicada em diversas aplicações, como por exemplo para gerenciamento do fluxo de um programa e também:
 
     . Avaliação de expresões e parsing sintático;
     . Algoritmos de Backtrancking;
@@ -13,4 +13,59 @@
     . Análise de expressões aritméticas.
 */
 
+
+// EXEMPLO DE PILHA
+
+
+// Nessa pilha estamos armazenando o nome e a idade de duas pessoas, primeiro foi criada uma classe chamada pilha, aonde é construido um array que irá ser usada como pilha. Logo após é implementado o método PUSH, na qual irá adicionar um elemento na pilha, esse método irá utilizar um argumento chamado people, que será o nosso objeto que armazenará o nome e a idade das pessoas, dentro do método push iremos adicionar o elemento para dentro do array que servirá como pilha. Em uma pilha existe vários métodos como o próprio push que já mencionei e outros como pop, isEmpty, peek, clear, isFull, size e entre outros. Dentro da classe pilha podemos colocar outros métodos para utilização futura no algoritmo;
+
+class Pilha
+{
+    constructor() // este método constructor, irá ser o responsável por criar os objetos myItems e countItems
+    {
+        this.myItems = []; // aqui é criado um array que servirá como base para a montagem da pilha
+        this.countItems = 0; // este é apenas um contador de quantos itens existe dentro da pilha.    
+    }
+
+    push(people) // este é o método que irá ser o responsável por adicionar um elemento a pilha.
+    {
+        this.myItems.push(people); // essa instrução é responsável por adicionar o objeto people para dentro da pilha. O objeto people será usado como base para a adição de elementos a pilha, a instrução chama o array myItems e adiciona o objeto people a ele, assim dizendo que instâncias do objeto people deverá ser adicionado ao myItems.
+        this.countItems++; // a cada adição de novos elementos a pilha, o contador será atualizado;
+    }
+
+    isEmpty() // esse é o método que retorna true caso a pilha esteja completamente vazia
+    {
+        return this.myItems.length === null; // essa instrução será responsável por verificar se a pilha está completamente vazia e irá retorna true caso ela esteja vazia, caso contrário, ela retornará false, assim tendo certeza de que a pilha contém elementos.
+    }   
+
+    display()
+    {
+        for(let i = this.myItems.length - 1; i >= 0; i--)
+        {
+            console.log(this.myItems[i]);
+            console.log(this.countItems);
+        }
+    }
+}
+
+function viewElements()
+{
+    let myStack = new Pilha;
+
+    let people1 = {
+        name: "Rafael",
+        age: 19
+    };
+    let people2 = {
+        name: "vitoria",
+        age: 22
+    }
+
+    myStack.push(people1);
+    myStack.push(people2);
+    myStack.display();
+    console.log(myStack.isEmpty());
+}
+
+viewElements();
 
